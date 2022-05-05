85e940462c1ceae04c3c523bc9f773be935c7172
Paul Tarvydas <guitarvydas@gmail.com>
2022-05-05 08:21:31 -0400

    begin _makechildren ()

--- json2js/testbench4.js
+++ json2js/testbench4.js
@@ -7,12 +7,16 @@ var Order_Taker_signature = {
 }
 
 
+
+function Order_Taker_makechildren (me) {
+      var child1 = new Phrase_Parser (me, "Phrase Parser");
+}
+
 var Order_Taker_protoImplementation = {
     name: "Order_Taker",
     kind: "container",
     begin: function () {},
     finish: function () {},
-      var child1 = new Phrase_Parser (me, "Phrase Parser")
     self.connections = [ conn2, conn3, conn4 ]
     handler: function (me, message) {
         deliverInputMessageToAllChildrenOfSelf (me, message);
@@ -21,6 +25,7 @@ var Order_Taker_protoImplementation = {
 
 function Order_Taker (container, instancename) {
     let me = new Container (Order_Taker_signature, Order_Taker_protoImplementation, container, instancename);
+    me.children = Order_Taker_makechildren (me);
     ...
     return me;
 }
