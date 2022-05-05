5df6776947862c7fe96a76f94effef1b4ffe2161
Paul Tarvydas <guitarvydas@gmail.com>
2022-05-05 08:07:15 -0400

    wip: jsize ; working on container implementation

---
+++ json2js/testbench4.js
@@ -0,0 +1,28 @@
+
+
+var Order_Taker_signature = {
+    name: "Order_Taker",
+    inputs: [{name:"phrase", structure: ["phrase"]}],
+    outputs: [{name:"food order", structure: ["food order"]}]
+}
+
+
+var Order_Taker_protoImplementation = {
+    name: "Order_Taker",
+    kind: "container",
+    begin: function () {},
+    finish: function () {},
+      var child1 = new 
+    self.connections = [ conn2, conn3, conn4 ]
+    handler: function (me, message) {
+        deliverInputMessageToAllChildrenOfSelf (me, message);
+    }
+}
+
+function Order_Taker (container, instancename) {
+    let me = new Container (Order_Taker_signature, Order_Taker_protoImplementation, container, instancename);
+    ...
+    return me;
+}
+
+
