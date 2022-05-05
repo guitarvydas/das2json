4607a3e758e8b36a54a5f5a7055fd2cfa460ef7a
Paul Tarvydas <guitarvydas@gmail.com>
2022-05-05 07:30:41 -0400

    move code to Leaf

--- json2js/testbench1.js
+++ json2js/testbench1.js
@@ -1,8 +1,14 @@
 
 
 var HTML_Button_signature = {
-  name: "HTML_Button",
-  inputs: [],
-  outputs: [{name:"click", structure: ["click"]}]
+    name: "HTML_Button",
+    inputs: [],
+    outputs: [{name:"click", structure: ["click"]}]
+}
+var HTML_Button_protoImplementation = {
+    name: "HTML_Button",
+    kind: "leaf",
+    begin: function () {},
+    finish: function () {}
 }
 
---
+++ json2js/testbench4.js
@@ -0,0 +1,8 @@
+
+
+var Order_Taker_signature = {
+  name: "Order_Taker",
+  inputs: [{name:"phrase", structure: ["phrase"]}],
+  outputs: [{name:"food order", structure: ["food order"]}]
+}
+
