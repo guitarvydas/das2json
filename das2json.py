e51aa409bcb93327c69bf37481e1a81d86b0edfe
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-05-04 07:59:19 -0400

    begin receptor revamp

--- das2json.py
+++ das2json.py
@@ -2,55 +2,70 @@ _in = None
 EOF = chr (0)
 
 
-def das2json ():
+def Das2json ():
     global _in
-    Spaces () # Spaces "<" Stuff
-    _in.need ("<")
-    _in.echo ()
+    _in.push_new_string ()
+    _in.begin_breadcrumb ("Das2json")
+    Spaces ()
+    _in.append_returned_string ()
+    _in.need_and_append ("<")
     Attributes ()
-    if _in.peek (">"):
-        _in.accept () # ">" ? Content "</" Stuff ">"
-        _in.echo ()
+    _in.append_returned_string (s)
+    if _in.maybe_append (">"):
         Content ()
-        _in.need ("</")
-        _in.echo ()
+        _in.append_returned_string ()
+        _in.need_and_append ("</")
         Stuff ()
-        _in.need (">")
-        _in.echo ()
-    elif _in.peek ("/>"):
-        _in.accept () # "/>" ?
-        _in.echo ()
+        _in.append_returned_string ()
+        _in.need_and_append (">")
+    elif _in.maybe_append ("/>"):
+        pass
     else:
-        _in.syntax_error ()
+        Stuff ()
+        _in.append_returned_string ()
+    _in.end_breadcrumb ("Das2json")
+    return _in.return_string_pop ()
 
 def Content ():
-  while True:
-      Spaces ()
-      if _in.peek ("</"):
-          break
-      elif _in.peek ("<"):
-          das2json ()
-      else:
-          Stuff ()
+    _in.push_new_string ()
+    _in.begin_breadcrumb ("Content")
+    while True:
+        Spaces ()
+        _in.append_returned_string ()
+        if _in.peek ("</"):
+            break
+        elif _in.peek ("<"):
+            Das2json ()
+            _in.append_returned_string ()
+        else:
+            Stuff ()
+            _in.append_returned_string ()
+    _in.end_breadcrumb ("Content")
+    return _in.return_string_pop ()
           
             
 def Attributes ():
+    _in.push_new_string ()
+    _in.begin_breadcrumb ("Attributes")
     while True:
         if _in.peek ("style="):
-            _in.accept ()
+            _in.accept_and_append ()
             String ()
-            _in.clear_collector ()
+            _in.append_returned_string ()
         elif _in.peek (">"):
             break
         elif _in.peek ("/>"):
             break
-        elif _in.peek (EOF):
+        elif _in.eof ():
             break
         else:
-            _in.accept ()
-            _in.echo ()
+            _in.accept_and_append ()
+    _in.end_breadcrumb ("Attributes")
+    return _in.return_string_pop ()
 
 def Stuff ():
+    _in.push_new_string ()
+    _in.begin_breadcrumb ("Stuff")
     while True:
         if _in.peek (">"):
             break
@@ -61,170 +76,181 @@ def Stuff ():
         elif _in.peek (EOF):
             break
         else:
-            _in.accept ()
-            _in.echo ()
+            _in.accept_and_append ()
+    _in.end_breadcrumb ("Stuff")
+    return _in.return_string_pop ()
 
 def Spaces ():
     global EOF
+    _in.push_new_string ()
+    _in.begin_breadcrumb ("Spaces")
     while True:
         if _in.peek (" "):
-            _in.accept ()
-            _in.echo ()
+            _in.accept_and_append ()
         elif _in.peek ("\t"):
-            _in.accept ()
-            _in.echo ()
+            _in.accept_and_append ()
         elif _in.peek ("\n"):
-            _in.accept ()
-            _in.echo ()
+            _in.accept_and_append ()
         elif _in.peek (EOF):
             break
         else:
             break
+    _in.end_breadcrumb ("Spaces")
+    return _in.return_string_pop ()
         
 def String ():
-    _in.need ('"')
-    _in.collect ()
+    _in.push_new_string ()
+    _in.begin_breadcrumb ("String")
+    _in.need_and_append ('"')
     NotDquotes ()
-    _in.need ('"')
-    _in.collect ()
+    _in.append_returned_string ()
+    _in.need_and_append ('"')
+    _in.end_breadcrumb ("String")
+    return _in.return_string_pop ()
 
 def NotDquotes ():
+    _in.push_new_string ()
+    _in.begin_breadcrumb ("NotDquotes")
     while True:
         if _in.peek ('"'):
             break
         else:
-            _in.accept ()
-            _in.collect ()
+            _in.accept_and_append ()
+    _in.end_breadcrumb ("NotDquotes")
+    return _in.return_string_pop ()
+
+
+
+
 
 
 #####
+
 import sys
 
-class Token:
+class InCharacter:
     def __init__ (self, c='', position=0):
         self.c = c
         self.position = position
+
+class Breadcrumb:
+    def __init__ (self, name, depth, pos):
+        self.name = name
+        self.depth = depth
+        self.position = pos
+        
+class CharacterStream:
+    # we put input characters in a cache 
+    # when pattern matching is successful, we grab (accept) the string in the cache and reset the cache
+    # when pattern matching is unsuccessful, we simply rewind the stream to the front and try again
     
-class InputStream:
     def __init__ (self):
         self.stdin_position = 0
-        self.most_recently_accepted = None
-        self.afresh ()
-        self.collector = ""
-        
-    def afresh (self):
-        self.cache = []
-        self.cache_index = -1
-        self.getc ()
-
-    def rewind (self):
-        self.cache_index = 0
+        self.cache = ""
+        self.cache_index = None
+        self.getc
 
     def getc (self):
-        if len (self.cache) == 0:
+        # ensure that the next character is in the cache
+        # if the cache is not empty and the index is in bounds, then the character is already in place
+        # else, get a character from stdin and append it to the cache
+        # for convenience EOF is a character and is defined above
+        if (len (self.cache) == 0) or (len (self.cache) == (self.cache_index + 1)):
             c = sys.stdin.read (1)
             if 1 > len (c):
                 c = EOF
             self.stdin_position += 1
-            self.cache = [Token (c=c, position=self.stdin_position)]
+            self.cache = [InCharacter (c=c, position=self.stdin_position)]
             self.cache_index = 0
-        elif len (self.cache) == (self.cache_index + 1):
-            c = sys.stdin.read (1)
-            self.stdin_position += 1
-            self.cache.append (Token (c=c, position=self.stdin_position))
+        elif self.cache [self.cache_index] != EOF:
             self.cache_index += 1
         else:
-            self.cache_index += 1
+            pass
 
-    def current_token (self):
+    def current_char (self):
         return self.cache [self.cache_index]
 
-    def echo (self) :
-        for tok in self.most_recently_accepted:
-            print (f'{tok.c}', end='')
-            
-    def collect (self) :
-        for tok in self.most_recently_accepted:
-            self.collector = self.collector + tok.c
-            
-    def echo_collection (self) :
-        print (f'{self.collector}', end='')
-        self.clear_collector ()
+    def rewind (self):
+        self.cache_index = 0
 
-    def clear_collector (self):
-        self.collector = ""
-            
+    def clear (self):
+        self.cache = ""
+        self.cache_index = None
+
     def accept (self):
-        # do something with cached tokens, then start afresh
-        # ...
-        self.most_recently_accepted = self.cache
-        if EOF == self.current_token ().c:
-            print ("\x1B[101m*** error: at EOF\x1B[0m")
-            sys.exit (1)
-        # ... start afresh
-        self.afresh ()
+        r = self.cache
+        self.clear ()
 
-    def syntax_error (self,s):
-        print (f'\x1B[101m*** syntax error wanted "{s}", but, got {self.ftok (self.current_token ())}\x1B[0m')
-        sys.exit (1)
 
-    def need (self, s):
-        if self.peek (s):
-            self.accept ()
-            return True
-        else:
-            self.syntax_error (s)
-            return False
+class Receptor:
+    # A receptor parses the input stream of characters and keeps track of how it got there and what it is working on.
+    # A receptor uses rules that can call other rules (so, we need to use stacks, to keep everything separated).
+    # Each rule begins a fresh string and builds it up.
+    # When a rule finishes, it pops its own string off of the string stack and puts it onto the return stack - the caller
+    #  deals with the returned string (typically by adding it to its own string). The caller must delete the returned value
+    #  from the return stack. Note that the caller can further parse the returned string, if it so wishes.
+
+    def __init__ (self):
+        self.instream = CharacterStream ()
+        self.string_stack = []
+        self.return_stack = []
+        self.breadcrumb_stack = []
+        self.breadcrumb_wip_stack = []
+        self.breadcrumb_wip_depth = 0
         
+    def push_new_string (self):
+        self.string_stack.append ("")
+        
+    def return_string_pop (self):
+            self.return_stack.append (self.string_stack.pop ())
+
+    def begin_breadcrumb (self, name):
+        self.breadcrumb_wip_depth += 1
+        b = Breadcrumb (name, self.breadcrumb_wip_depth, self.current_input_position ())
+        self.breadcrumb_wip_stack.append (b)
+        
+    def end_breadcrumb (self, name):
+        self.breadcrumb_stack.append (self.breadcrumb_wip_stack.pop ())
+        self.breadcrumb_wip_depth -= 1
+
+    def append (self, s):
+        self.string_stack [-1].append (s)
+        
+    def accept_and_append (self):
+        s = self.instream.accept ()
+        self.append (s)
+
     def peek (self, s):
-        if self.peek_rec (s):
-            self.rewind ()
+        if self.peek_recursively (s):
+            self.instream.rewind ()
             return True
         else:
-            self.rewind ()
+            self.instream.rewind ()
             return False
-        
-    def peek_rec (self, s):
-        global EOF
-        if 0 == len (s):
-            if EOF == self.current_token ().c:
-                return True
-            else:
-                return False
-        elif s [0] == self.current_token ().c:
-            if 1 == len (s):
-                return True
-            else:
-                self.getc ()
-                return self.peek_rec (s [1:])
+
+    def append_returned_string (self):
+        s = self.return_stack.pop ()
+        self.append (s)
+
+    def need_and_append (self, s):
+        if self.peek (s):
+            self.append (s)
+        else:
+            self.error (s)
+
+    def maybe_append (self, s):
+        if self.peek (s):
+            self.append (s)
+            return True
         else:
             return False
 
-    def ftok (self, tok):
-        global EOF
-        c = tok.c
-        if c == ' ':
-            c = "' '"
-            h = hex (ord (tok.c))
-        elif c == '\t':
-            c = "<unprintable>"
-            h = hex (ord (tok.c))
-        elif c == '\n':
-            c = "<unprintable>"
-            h = hex (ord (tok.c))
-        elif c == EOF:
-            c = "*EOF*"
-            h = "?"
-        elif c == "":
-            c = "<empty>"
-            h = "?"
-        else:
-            h = hex (ord (tok.c))
-        return f'{c} {h} at {tok.position}'
+    def error (s):
+        print (f'\x1B[101mReceptor error at input position {self.breadcrumb_wip_stack [-1].position}\x1B[0m')
 
 def _begin ():
     global _in
     _in = InputStream ()
     
 _begin ()
-das2json ()
+Das2json ()
