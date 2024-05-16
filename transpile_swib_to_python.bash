ffe653dccab9aa3637dbb22d845caec513a4cc72
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-05-16 06:33:38 -0400

    begin simplifying Makefile

---
+++ transpile_swib_to_python.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+# builds das2json.py from diagram (das2json.drawio)
+# diagram -> das2json.py uses bootstrapping py0D
+0D/das2json/das2json 0D/python/std/transpile.drawio
+0D/das2json/das2json das2json.drawio
+python3 py0d.py . 0D/python das2json.swib main das2json.drawio.json transpile.drawio.json
